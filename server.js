9d5c7950ed0230d8436a9d203a8fa0ee3e7f45b7
SHANZA SHIRIN K C <shnzshirin@gmail.com>
2026-02-06 12:47:40 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+const app = require("./src/app");
+const connectDB = require("./config/db");
+
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`);
+});
